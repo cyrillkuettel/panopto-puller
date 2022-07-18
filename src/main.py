a87e7c10fe8750d1921507c36e700b1f38552edd
Cyrill <cyrillkuettel@gmail.com>
2022-07-18 17:11:38 +0200

    add default action for button

--- src/main.py
+++ src/main.py
@@ -4,15 +4,16 @@ import os
 import yt_dlp
 from PyQt6.QtCore import *
 from PyQt6.QtCore import QThread
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon, QAction
+from PyQt6.QtWidgets import QApplication, QWidget, QMenu, QToolButton
+
 from PyQt6.QtWidgets import QPushButton, QLabel, QLineEdit, QProgressBar, QVBoxLayout, QHBoxLayout, QFileDialog
 from src.models import Cookie
 from src.Utils import get_new_value
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))  # PYTHONPATH needs to be inserted. This enables running this script
-# from the command line, from any directoy.
+# from the command line, from any directory.
 logging.basicConfig(level=logging.DEBUG)
 Log = logging.getLogger(__name__)
 
@@ -20,6 +21,8 @@ Log = logging.getLogger(__name__)
 class Window(QWidget):
     def __init__(self):
         super().__init__()
+        self.label_url = None
+        self.menu = None
         self.le_file_path = None
         self.btn_download = None
         self.btn_file_path = None
@@ -72,7 +75,20 @@ class Window(QWidget):
         self.btn_cookies.setIcon(QIcon('icons:cookies.ico'))
         self.btn_file_path = QPushButton()
         self.btn_file_path.setIcon(QIcon('icons:choose-file-icon-16.png'))
-        self.btn_download = QPushButton('Download', self)
+        self.menu = QMenu()
+        self.menu.addAction('test1')
+        self.menu.addAction('test2')
+        self.btn_download = QToolButton(self)
+        self.btn_download.setMenu(self.menu)
+        # self.btn_download.setText("Download")
+
+        button_action = QAction("Download", self)
+        self.btn_download.setPopupMode(QToolButton.ToolButtonPopupMode.MenuButtonPopup)
+        mode = self.btn_download.popupMode()
+        Log.warning(str(mode))  
+        button_action.triggered.connect(self.on_click_default_action)
+        self.btn_download.setDefaultAction(button_action)
+
         self.le_file_path = QLineEdit(application_path)
         self.le_file_path.setMaximumWidth(200)
         self.le_url = QLineEdit()
@@ -106,21 +122,26 @@ class Window(QWidget):
         file_path = QFileDialog.getExistingDirectory()
         self.le_file_path.setText(file_path)
 
+    def on_click_default_action(self):
+        Log.warning("Default action triggered.")
+
     def on_click_download_button(self):
-        if self.check_valid_input():
+        if self.valid_input() and self.cookie_loaded():
             self.thread.start()
 
-    def check_valid_input(self):
-        if self.cookie is None:
-            self.status_info_label.setText('No cookies file loaded')
-            self.status_info_label.setStyleSheet('color: red')
-            Log.error("Need to provide a cookie")
-            return False
+    def valid_input(self):
         if self.le_url.text() == '':
             Log.error("No URL provided")
             self.status_info_label.setText('No URL provided')
             return False
         return True
+
+    def cookie_loaded(self):
+        if self.cookie is None:
+            self.status_info_label.setText('No cookies file loaded')
+            self.status_info_label.setStyleSheet('color: red')
+            Log.error("Need to provide a cookie")
+            return False
 
     def start_download_with_cookie(self):
 
@@ -176,7 +197,7 @@ class Window(QWidget):
     # noinspection PyUnresolvedReferences
     def setup_onclick_listeners(self):
         self.btn_file_path.clicked.connect(self.choose_download_destination)
-        self.btn_download.clicked.connect(self.on_click_download_button)
+        # self.btn_download.clicked.connect(self.on_click_download_button)
         self.btn_cookies.clicked.connect(self.open_cookie_file)
 
 
